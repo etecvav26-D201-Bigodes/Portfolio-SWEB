0485bd28b7bd995e37ad9d5406e0bad642994712
Laura Duarte <dduartelaureta@gmail.com>
2026-03-27 16:50:53 -0300

    exercicio 5 php

---
+++ LauraDuarte-Nicolas/exercicios-php/exercicio.5/calcula.php
@@ -0,0 +1,49 @@
+<?php
+
+$n1 = $_POST["n1"];
+$n2 = $_POST["n2"];
+$n3 = $_POST["n3"];
+$n4 = $_POST["n4"];
+$n5 = $_POST["n5"];
+
+$soma = 0;
+
+?>
+
+<!DOCTYPE html>
+
+<html lang="pt-br">
+
+<head>
+
+<meta charset="UTF-8">
+
+<title>Resultado (POST)</title>
+
+</head>
+
+<body>
+
+<h1>Resultado da Soma dos Fatoriais (POST)</h1>
+
+<?php
+function fatorial($num) {
+    $fat = 1;
+
+    for ($i = 1; $i <= $num; $i++) {
+        $fat = $fat * $i;
+    }
+
+    return $fat;
+}
+
+$soma = fatorial($n1) + fatorial($n2) + fatorial($n3) + fatorial($n4) + fatorial($n5);
+?>
+
+<p> A soma dos fatoriais é <?php echo $soma; ?> </p>
+
+<a href="exercicio5.php">Voltar</a>
+
+</body>
+
+</html>
---
+++ LauraDuarte-Nicolas/exercicios-php/exercicio.5/exercicio5.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Fatoriais</title>
+</head>
+<body>
+
+<h2>Digite 5 números</h2>
+
+<form action="calcula.php" method="POST">
+
+    Número 1: <input type="number" name="n1" required><br><br>
+    Número 2: <input type="number" name="n2" required><br><br>
+    Número 3: <input type="number" name="n3" required><br><br>
+    Número 4: <input type="number" name="n4" required><br><br>
+    Número 5: <input type="number" name="n5" required><br><br>
+
+    <input type="submit" value="Calcular">
+
+</form>
+
+</body>
+</html>
